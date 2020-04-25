c8626f9efe420ec70552b964ad439fcd5fa608d9
James Evans <jevans@itsjevans.co.uk>
2020-04-25 16:45:27 +0100

    Initial Commit

---
+++ Floral/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Floral
+//
+//  Created by James Evans on 25/04/2020.
+//  Copyright © 2020 James Evans. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
